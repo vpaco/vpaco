639edbe629ea42a5e8051812dd9ccdf8ddcc72e7
liu.jun <askliujun@163.com>
2020-10-28 21:59:48 +0800

    页面config支持es6写法

--- src/components/pageInner.vue
+++ src/components/pageInner.vue
@@ -140,7 +140,9 @@
                 this._initAncestorRefs();
 
                 let configCallback = rawPageConfig.config;
-                let config = configCallback ? configCallback(this) : {};
+                let config = configCallback ? (()=>{
+                    return configCallback.bind(this)(this);
+                })() : {};
 
                 let {options, events, methods, init, mounted, optionsChange, destroy} = config;
 
