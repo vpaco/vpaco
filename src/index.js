d31cbf220d8308134f32b37a66a100d676d6d2a4
liuj1 <liuj1@t3go.cn>
2021-01-27 11:14:52 +0800

    新增export loadRemoteModule，移除default导出

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { register as _register, setVue, addPage as _addPage } from './config';
+import {loadRemoteModule as _loadRemoteModule} from './utils';
 import Page from './components/page';
 import Component from './components/component';
 
@@ -6,6 +7,7 @@ export const VpPage = Page;
 export const VpComponent = Component;
 export const register = _register;
 export const addPage = _addPage;
+export const loadRemoteModule = _loadRemoteModule;
 export const install = function (Vue, opts = {}) {
     Vue.component('VpPage', VpPage);
     Vue.component('VpComponent', VpComponent);
@@ -16,11 +18,3 @@ export const install = function (Vue, opts = {}) {
 if (typeof window !== 'undefined' && window.Vue) {
     install(window.Vue);
 }
-
-export default {
-    install,
-    register,
-    VpPage,
-    VpComponent,
-    addPage
-};
